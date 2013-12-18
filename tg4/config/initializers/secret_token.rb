ea43290463a298691f0789cd2bdb36106207b073
rails <rails@centos.onuki>
2013-12-19 00:40:21 +0900

    init and change 'Gemfile'.

---
+++ tg4/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tg4::Application.config.secret_token = '72154a693db1a083b53d83bf5422d8b2533d8e82ceec345ef5f88836883b5f24972d3c1d94bdd8089fafb106e83884aa70897bf7d968b5ad1ee33e7fda2cf6f7'
