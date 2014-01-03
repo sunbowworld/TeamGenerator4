1c9eac30d13d9d48b99a8c6ef2ab88d8a930ebd5
k_rororo <heavens.feel.7@gmail.com>
2014-01-04 02:32:47 +0900

    テスト、ヘルパー、SCSS を作らないよう変更

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module Tg4
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.test_framework false
+      g.assets false
+      g.helper false
+    end
   end
 end
